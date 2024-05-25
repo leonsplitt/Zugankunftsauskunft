a0b888c6f82e4342724f3714dbfacc9437fe71bc
Leon Splitt <splittleon@gmail.com>
2024-05-25 02:06:42 +0200

    Show time as minutes when under 15 minutes

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
 from datetime import datetime
-import requests
 from typing import Any
+import requests
 
 
 def show_stop(stop_id: int):
     r = requests.get(
         f"https://v6.bvg.transport.rest/stops/{stop_id}/departures",
-        params= { "results": 5, "duration": 60 }
+        params={"results": 5, "duration": 240},
     )
     departures: list[dict[str, Any]] = r.json()["departures"]
-    for d in departures:
-        show_departure(d)
 
-def show_departure(departure): 
-    when = datetime.fromisoformat(departure["when"])
+    current_time = datetime.now()
+    for d in departures:
+        show_departure(d, current_time)
+
+
+def show_departure(departure, current_time: datetime):
+    when = format_time(departure["when"], current_time)
     direction = departure["destination"]["name"].removesuffix(" (Berlin)")
     name = departure["line"]["name"]
-    print(f"Hello! The {name} to {direction} is coming at {when.hour:02}:{when.minute:02}")
+    print(f"Hello! The {name} to {direction} is coming {when}")
+
+
+def format_time(iso_time: str, current_time: datetime) -> str:
+    time = datetime.fromisoformat(iso_time)
+    current_time = current_time.astimezone(time.tzinfo)
+
+    deltatime = time - current_time
+    deltaminutes = int(deltatime.total_seconds() / 60)
+
+    if deltaminutes < 0:
+        return "now"
+    elif deltaminutes < 15:
+        return f"in {deltaminutes} minutes"
+    else:
+        return f"at {time.hour:02}:{time.minute:02}"
+
 
 def main():
     stops = [
-        900078201, # S Neukölln
-        900110521, # Hufeland
-        900110520, # Bötzowstr.
+        900078201,  # S Neukölln
+        900110521,  # Hufeland
+        900110520,  # Bötzowstr.
     ]
-    for i in stops:
-        show_stop(stop_id=i)
+    for stop_id in stops:
+        show_stop(stop_id)
         print()
+
 
 main()
